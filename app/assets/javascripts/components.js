6bf10f18b4b2ac87db644b640e55b2c89ead8f89
Maximilian Schulz <m.schulz@kulturfluss.de>
2015-02-19 09:15:06 +0100

    Create a demo component and render it to the page

---
+++ app/assets/javascripts/components.js
@@ -0,0 +1,8 @@
+//= require_self
+
+// Load react itself
+React = require('react');
+
+// All components
+DemoComponent = require('./components/demo_component');
+
---
+++ app/assets/javascripts/components/demo_component.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var DemoComponent = React.createClass({
+  render: function() {
+    return <div>Demo Component</div>;
+  }
+});
+
+module.exports = DemoComponent;
